6b270dfe369678ff2396fb788fb0bae1eaa13b76
Favour Umejesi <umejesif@gmail.com>
2025-07-15 11:38:25 -0400

    Time: 73 ms (96.29%), Space: 27.6 MB (94.32%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,21 +6,33 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        #Optimal Approach
+        if not headA or not headB:
+            return None
+            
+        l1 = headA
+        l2 = headB
 
-        #Brute force approach
-        seen = set()
-        current = headA
+        while l1 != l2:
+            l1 = l1.next if l1 else headB
+            l2 = l2.next if l2 else headA
 
-        # Add all values from headA to the set
-        while current:
-            seen.add(current)
-            current = current.next
+        return l2
 
-        # Check for first common value in headB
-        curr = headB
-        while curr:
-            if curr in seen:
-                return curr
-            curr = curr.next
+        # #Brute force approach
+        # seen = set()
+        # current = headA
 
-        return None  
+        # # Add all values from headA to the set
+        # while current:
+        #     seen.add(current)
+        #     current = current.next
+
+        # # Check for first common value in headB
+        # curr = headB
+        # while curr:
+        #     if curr in seen:
+        #         return curr
+        #     curr = curr.next
+
+        # return None  
