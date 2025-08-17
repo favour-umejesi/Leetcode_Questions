a99289dc910224a764af08e7ed2593a7b49f9096
Favour Umejesi <umejesif@gmail.com>
2025-08-16 19:54:23 -0500

    Time: 0 ms (100%), Space: 17.8 MB (36.69%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.py
+++ 0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -3,26 +3,44 @@ U - if ch # in string, once found, delete character before it, then check if wha
 M - String, Stack
 P - 1) Create two stacks for each of the strings
   - 2) Tranverse through each string
+        - check if char is != "#":
+        - If so, append char to stack
+        - else, we check if stack is not empty and pop last element
+    3) After repeating for second string, compare the two stacks
 """
 
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        stackS = []
-        stackT = []
+
+        def helper(word):
+            stack = []
+            for char in word:
+                if char != "#":
+                    stack.append(char)
+                else:
+                    if stack:
+                        stack.pop()
+            return stack
+
+        return helper(s) == helper(t)
+
         
-        for char in s:
-            if char != "#":
-                stackS.append(char)
-            else:
-                if stackS:
-                    stackS.pop()
+        # stackS = []
+        # stackT = []
+        
+        # for char in s:
+        #     if char != "#":
+        #         stackS.append(char)
+        #     else:
+        #         if stackS:
+        #             stackS.pop()
 
-        for char in t:
-            if char != "#":
-                stackT.append(char)
-            else:
-                if stackT:
-                    stackT.pop()
+        # for char in t:
+        #     if char != "#":
+        #         stackT.append(char)
+        #     else:
+        #         if stackT:
+        #             stackT.pop()
 
 
-        return stackS == stackT
+        # return stackS == stackT
