ebd1e22c3bdcfa1b95542d7cad9b4afdbea3a801
Favour Umejesi <umejesif@gmail.com>
2025-08-19 11:11:36 -0500

    Time: 332 ms (44.17%), Space: 19.8 MB (24.14%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -1,29 +1,23 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        """
-        U- if letter in string frequency is greater than 2, delete other occurences of it making sure we only have the minimum possible number of
-        characters, not > 2
-        Plan:
-        keep count, two pointers, set one to 0, the other to 1
-        create an empty string to keep characters that fall under the condition
-        if l == r, increase count, else sent frequency to 1
-        if count > 2, continue to skip element after 2 then add to the list, return list
-        """
-        res = ""
-        l, r, c = 0, 1, 0
 
-        for r in range(len(s)):
-            if s[l] == s[r]:
-                c += 1
-            else:
-                c = 1
-            l = r
+        stack = []
+        count = 1
+
+        for letter in s:
+            if stack and stack[-1] == letter:
+                count += 1
+            if stack and stack[-1] != letter:
+                count = 1
+
         
-            if c > 2:
+            if count >= 3:
                 continue
             
-            res += s[l]
-        
-        return res
+            else:
+                stack.append(letter)
+
+        return "".join(stack)
+
 
         
