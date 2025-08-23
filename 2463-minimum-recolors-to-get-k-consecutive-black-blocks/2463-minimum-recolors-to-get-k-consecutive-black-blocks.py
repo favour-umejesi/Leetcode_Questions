34924474d31fe1e8f21eab6bdb339517e36d9b77
Favour Umejesi <umejesif@gmail.com>
2025-08-23 17:56:51 -0500

    Time: 0 ms (100%), Space: 17.7 MB (61.46%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -3,8 +3,16 @@ class Solution:
         """
         U - return the minimum number of times it takes to change white blocks to black to get k consecutive number of black blocks
         M - String, Sliding window
-        P - create 
-
+        P - create two variables, one to keep track of W's and other keeps track of the maximum number of W's we have seen so far
+        - Build the first window by looping through the first k elements
+                - counts how many W's are in the window and stores count
+                - sets ans == count, to store the min we have seen so far
+        - Slide the window
+                - For each character at S[i], we
+                - remove the effect of the character that left the window, s[i-k]
+                - Add the effect of the new character at s[i]
+        - Update the ans with the minimum W's count so far
+        - Return ans
         """
         count, ans = 0, 0
         for i in range(k):
@@ -16,7 +24,6 @@ class Solution:
                 count -= 1
             if blocks[i] == "W":
                 count += 1
-            
             
             ans = min(ans, count)
 
