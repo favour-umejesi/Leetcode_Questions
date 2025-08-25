3a481df857f7526b49b583a976369d069df04ce6
Favour Umejesi <umejesif@gmail.com>
2025-08-24 21:04:27 -0500

    Time: 52 ms (65.77%), Space: 19.7 MB (83.35%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -19,7 +19,7 @@ class Solution:
         for right in range(len(nums)):
             product *= nums[right]
 
-            while product >= k and left <= right:
+            while product >= k:
                 product //= nums[left]
                 left += 1
             
