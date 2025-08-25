93efa964b996463b53aafb9a9c872473e5426761
Favour Umejesi <umejesif@gmail.com>
2025-08-25 17:13:06 -0500

    Time: 1 ms (66.47%), Space: 18 MB (19.92%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -2,22 +2,30 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         """
         Return the Top K most frequent words in the array
-        M - Array, Sort
+        M - Array, Bucket Sort
         """
         seen = {}
+        n = len(words)
         for word in words:
             seen[word] = 1 + seen.get(word, 0)
 
-        freq = []
-        for key, value in seen.items():
-            freq.append([-value, key])
-        
-        freq.sort()
-        print(f"Frequecy {freq}")
-        result = []
-        while len(result) < k:
-            result.append(freq.pop(0)[1])
-        return result
+        bucket = [[] for _ in range(n+1)]
 
+        for key, value in seen.items():
+            bucket[value].append(key)
+        
+        result = []
+        for i in range(n, -1, -1):
+            if bucket[i]:
+                bucket[i].sort()
+                for word in bucket[i]:
+                    result.append(word)
+
+                    if len(result) == k:
+                        return result
+
+
+        
+        
 
         
