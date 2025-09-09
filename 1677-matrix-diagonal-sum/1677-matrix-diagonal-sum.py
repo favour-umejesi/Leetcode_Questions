3ebb8570b5685af4714df0352777b49acd5bd39e
Favour Umejesi <umejesif@gmail.com>
2025-09-09 18:16:40 -0500

    Time: 0 ms (100%), Space: 18 MB (98.5%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.py
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.py
@@ -1,12 +1,5 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
-
-        """
-        in the primary diagonal, the index of the row and column are the same
-        for the secondary diagonal, the row is the same but column keeps decreasing by 1
-        to ensure we don't add the intersection point value twice, we substract it from the final answer
-        """
-
         n = len(mat)
        
         res = 0
@@ -15,6 +8,6 @@ class Solution:
             res += mat[i][n-1-i] #secondary diagonal
 
         if n % 2 != 0: #if the matrix is odd
-            res -= mat[n//2][n//2]
+            res -= mat[n//2][n//2] #subtract value at intersection point
         return  res
         
