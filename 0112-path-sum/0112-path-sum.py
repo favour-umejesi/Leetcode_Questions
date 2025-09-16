d13d10cedd23cddbcbad5b08adfd1c3478f92305
Favour Umejesi <umejesif@gmail.com>
2025-09-16 02:31:17 -0500

    Time: 0 ms (100%), Space: 19 MB (22.92%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -1,10 +1,3 @@
-"""
-U - Find a path in the tree, where the root-leaf sums up to target
-M - Binary Tree
-P - 1) if there is no root, return false
-    2) if we have reached the end of the tree, left or right, we check if the node value is equal to what is left of target.
-    3) hasPathSum(root.left, targetSum - root.val) or hasPathSum(root.right, targetSum - root.val)
-"""
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -15,14 +8,13 @@ class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
-
+        
         if not root.left and not root.right:
             return root.val == targetSum
 
         return (
             self.hasPathSum(root.left, targetSum - root.val)
             or
-            self.hasPathSum(root.right, targetSum - root.val)
-        )
-    
+            (self.hasPathSum(root.right, targetSum - root.val))
+            )
         
