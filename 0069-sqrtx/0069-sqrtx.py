ddc2d5ac8b8ac646bb2471e6827e2f1e3b4c29c2
Favour Umejesi <umejesif@gmail.com>
2025-10-12 21:57:37 -0500

    Time: 4 ms (40.51%), Space: 17.9 MB (32.14%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,5 +1,21 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-
-        return int(x**0.5)
+        """
+        Using binary search over a range of 
+        """
+        first, last = 1, x
+        if x is 0:
+            return 0
         
+        while first <= last:
+            mid = first + (last - first) // 2
+            if mid == x // mid:
+                return mid
+
+            elif mid > x // mid:
+                last = mid - 1
+
+            else:
+                first = mid + 1
+        
+        return last
