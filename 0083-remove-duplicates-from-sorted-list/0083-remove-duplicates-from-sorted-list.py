d39f186015ed92cda74a6b22b7dd3363ba37cfaf
Favour Umejesi <umejesif@gmail.com>
2025-10-29 23:49:15 -0500

    Time: 0 ms (100%), Space: 18 MB (21.72%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -10,8 +10,8 @@ class Solution:
         while curr and curr.next:
             if curr.val == curr.next.val:
                 curr.next = curr.next.next
+
             else:
                 curr = curr.next
 
         return head
-        
