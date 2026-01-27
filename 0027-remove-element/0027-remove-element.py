aabced0bd28b01a09bbe5306b875712243d10716
Favour Umejesi <umejesif@gmail.com>
2026-01-27 05:45:58 -0600

    Time: 0 ms (100%), Space: 19.4 MB (22.44%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,11 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        j = 0
+        j = 0 #use to keep track of where the next (non-val) element should be placed
 
         for i in range(len(nums)):
             if nums[i] != val:
-                nums[j] = nums[i]
+                nums[j] = nums[i] # if it is not equal to val, replace the curr number with the number at position j, then move j, if not skip
                 j += 1
-
+        
         return j
         
