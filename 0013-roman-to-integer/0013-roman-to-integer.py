a345b9a54c0a96088d77f037242be7b2e76fca1e
Favour Umejesi <umejesif@gmail.com>
2026-01-27 05:12:29 -0600

    Time: 0 ms (100%), Space: 19.4 MB (8.21%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,14 +1,7 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        """
-        Given roman numerals, convert it to integer
-        Plan:
-        - Create a dictionary containing the roman numerals, mapped to integers
-        - looping through the string, we check if the first roman integer we encounter is less than the one after it
-            - if so, we substract its value from total
-            - else, we keep adding
-        """
-        roman = {
+
+        numerals = {
             "I": 1,
             "V": 5,
             "X": 10,
@@ -19,9 +12,14 @@ class Solution:
         }
         total = 0
         for i in range(len(s)-1):
-            if roman[s[i]] < roman[s[i+1]]:
-                total -= roman[s[i]]
+            if numerals[s[i]] < numerals[s[i+1]]:
+                total -= numerals[s[i]]
+
             else:
-                total += roman[s[i]]
-        total += roman[s[-1]]
+                total += numerals[s[i]]
+
+        total += numerals[s[-1]]
+
         return total
+
+        
